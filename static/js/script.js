032875d4de6fd40db0d9160280e105bc8cc5dcd8
KhashayarS <39525313+KhashayarS@users.noreply.github.com>
2025-04-30 10:19:47 +0330

    Change variable naming from snake_case to camelCase

--- static/js/script.js
+++ static/js/script.js
@@ -1,98 +1,98 @@
 function createGameboard() {
 
-    const board_current_result = [
+    const boardCurrentResult = [
         [null, null, null],
         [null, null, null],
         [null, null, null]
     ];
 
-    const getCurrentResult = () => board_current_result;
+    const getCurrentResult = () => boardCurrentResult;
 
-    function playTurn(value, row_index, column_index) {
+    function playTurn(value, rowIndex, columnIndex) {
         if (value !== 'x' && value !== 'o') {
             // only 'x' and 'o' are accepted as input values
             console.log('Input value should be either "x" or "o."');
             return;
-        } else if (board_current_result[row_index][column_index] !== null) {
+        } else if (boardCurrentResult[rowIndex][columnIndex] !== null) {
             console.log('The position has already been taken!');
             return;
         }
-        board_current_result[row_index][column_index] = value;
-        // Check there is a winner (commented out, will be check later in the round_result)
-        // checkRoundWinner(board_current_result);
+        boardCurrentResult[rowIndex][columnIndex] = value;
+        // Check there is a winner (commented out, will be check later in the roundResult)
+        // checkRoundWinner(boardCurrentResult);
         return;
     };
 
     // Helper functions
     function transposeBoard(board) {
 
-        const transposed_board = [];
+        const transposedBoard = [];
 
-        const num_rows = board.length;
-        const num_columns = board[0].length;
+        const numRows = board.length;
+        const numColumns = board[0].length;
         
-        let current_row = [];
-        for (let i = 0; i < num_rows; i++) {
+        let currentRow = [];
+        for (let i = 0; i < numRows; i++) {
             // Empty the row
-            current_row = [];
-            for (let j = 0; j < num_columns; j++) {
-                current_row.push(board[j][i]);
+            currentRow = [];
+            for (let j = 0; j < numColumns; j++) {
+                currentRow.push(board[j][i]);
             }
-            transposed_board.push(current_row);
+            transposedBoard.push(currentRow);
         }
 
-        console.transposed_board;
+        console.transposedBoard;
 
-        return transposed_board;
+        return transposedBoard;
 
     }
 
     function getMainDiagonal(board) {
-        const main_diagonal = [];
-        const num_rows = board.length;
+        const mainDiagonal = [];
+        const numRows = board.length;
     
-        for (let i = 0; i < num_rows; i++) {
-            main_diagonal.push(board[i][i]);
+        for (let i = 0; i < numRows; i++) {
+            mainDiagonal.push(board[i][i]);
         }
     
         // For compatibility, return it as a 1D array inside another array (like a 1D matrix)
-        main_diagonal_array = [main_diagonal];
+        mainDiagonalArray = [mainDiagonal];
 
-        return main_diagonal_array;
+        return mainDiagonalArray;
 
     }
 
     function getSecondaryDiagonal(board) {
-        const secondary_diagonal = [];
-        const num_rows = board.length;
+        const secondaryDiagonal = [];
+        const numRows = board.length;
     
-        for (let i = 0; i < num_rows; i++) {
-            secondary_diagonal.push(board[i][num_rows - 1 - i]);
+        for (let i = 0; i < numRows; i++) {
+            secondaryDiagonal.push(board[i][numRows - 1 - i]);
         }
     
         // For compatibility, return it as a 1D array inside another array (like a 1D matrix)
-        secondary_diagonal_array = [secondary_diagonal];
+        secondaryDiagonalArray = [secondaryDiagonal];
 
-        return secondary_diagonal_array;
+        return secondaryDiagonalArray;
     }
 
-    function checkThreeConsectives(input_array) {
+    function checkThreeConsectives(inputArray) {
 
         // This function only check for consecutive elements in a row, so for columns for example, first you should transpose them and then send them as inputs to this function
 
-        const num_rows = input_array.length;
-        const num_columns = input_array[0].length;
+        const numRows = inputArray.length;
+        const numColumns = inputArray[0].length;
 
-        for (const current_mark of ['x', 'o']) {
+        for (const currentMark of ['x', 'o']) {
 
             let counter = 0;
 
-            for (let i = 0; i < num_rows; i++) {
-                for (let j = 0; j < num_columns; j++) {
-                    if (input_array[i][j] === current_mark) { counter++ };
+            for (let i = 0; i < numRows; i++) {
+                for (let j = 0; j < numColumns; j++) {
+                    if (inputArray[i][j] === currentMark) { counter++ };
                     if (counter >= 3) {
 
-                        console.log(`Player with the mark "${current_mark}" Wins!`);
+                        console.log(`Player with the mark "${currentMark}" Wins!`);
 
                         // return the row/column/diagonal index
                         return i;
@@ -110,91 +110,90 @@ function createGameboard() {
 
     function checkRoundWinner() {
 
-        let round_finished = false;
+        let isRoundFinished = false;
 
-        const actual_board = [...board_current_result];
-        const row_completed = checkThreeConsectives(actual_board);
+        const actualBoard = [...boardCurrentResult];
+        const rowCompleted = checkThreeConsectives(actualBoard);
         
-        const transposed_board = transposeBoard(actual_board);
-        const column_completed = checkThreeConsectives(transposed_board);
+        const transposedBoard = transposeBoard(actualBoard);
+        const columnCompleted = checkThreeConsectives(transposedBoard);
         
-        const main_diagonal_array = getMainDiagonal(actual_board);
-        const main_diagonal_completed = checkThreeConsectives(main_diagonal_array);
+        const mainDiagonalArray = getMainDiagonal(actualBoard);
+        const mainDiagonalCompleted = checkThreeConsectives(mainDiagonalArray);
         
-        const secondary_diagonal_array = getSecondaryDiagonal(transposed_board);
-        const secondary_diagonal_completed = checkThreeConsectives(secondary_diagonal_array);
+        const secondaryDiagonalArray = getSecondaryDiagonal(transposedBoard);
+        const secondaryDiagonalCompleted = checkThreeConsectives(secondaryDiagonalArray);
 
         if (
-            row_completed === null &&
-            column_completed === null &&
-            main_diagonal_completed === null &&
-            secondary_diagonal_completed === null
+            rowCompleted === null &&
+            columnCompleted === null &&
+            mainDiagonalCompleted === null &&
+            secondaryDiagonalCompleted === null
         ) {
-            round_finished = false;
+            isRoundFinished = false;
         } else {
-            round_finished = true;
+            isRoundFinished = true;
         }
 
         return {
-            round_finished,
+            isRoundFinished,
         }
 
     }
 
     function resetBoard() {
         
-        for (let i = 0; i < board_current_result.length; i++) {
-            for (let j = 0; j < board_current_result[i].length; j++) {
-                board_current_result[i][j] = null;
+        for (let i = 0; i < boardCurrentResult.length; i++) {
+            for (let j = 0; j < boardCurrentResult[i].length; j++) {
+                boardCurrentResult[i][j] = null;
             }
         }
 
     }
 
     function beginRound(user1, user2) {
-        let round_finished = false;
+        let isRoundFinished = false;
         let player = user1;
-        let round_result = null;
+        let roundResult = null;
 
         resetBoard();
 
-        let new_input = 'empty';
-        while (!round_finished, new_input !== null) {
-            const player_name = player.getName();
-            const player_id = player.getID();
-            const player_mark = player.getMark();
+        let newInput = 'empty';
+        while (!isRoundFinished, newInput !== null) {
+            const playerName = player.getName();
+            const playerID = player.getID();
+            const playerMark = player.getMark();
 
-            console.log(`Player ${player_name}, it's your turn! Here is the current board, Pick one of the empty cells!`);
-            console.log(board_current_result);
-            new_input = prompt(`${player_name}! Input two digits separated by comma, i.e. the row and column numbers (beginning from 0)\ne.g. 0, 2`);
+            console.log(`Player ${playerName}, it's your turn! Here is the current board, Pick one of the empty cells!`);
+            console.log(boardCurrentResult);
+            newInput = prompt(`${playerName}! Input two digits separated by comma, i.e. the row and column numbers (beginning from 0)\ne.g. 0, 2`);
 
-            let user_input;
+            let userInput;
             try {
-                user_input = new_input.split(",").map(el => Number(el));
+                userInput = newInput.split(",").map(el => Number(el));
             } catch (error) {
                 throw (`The input format is not accpetable:\n${error}`);
             }
             
-            playTurn(player_mark, user_input[0], user_input[1]);
+            playTurn(playerMark, userInput[0], userInput[1]);
 
             // Check finishing of the game
-            round_result = checkRoundWinner();
-            console.log({round_result});
-            round_finished = round_result.round_finished;
+            roundResult = checkRoundWinner();
+            isRoundFinished = roundResult.isRoundFinished;
 
-            if (round_finished) {
+            if (isRoundFinished) {
                 console.log(`Round is finished! Winner: ${player.getName()}`);
-                if (player_id === user1.getID()) {
-                    let new_score = user1.getScore() + 1;
-                    user1.updateScore(new_score)
-                } else if (player_id === user2.getID()) {
-                    let new_score = user2.getScore() + 1;
-                    user2.updateScore(new_score)
+                if (playerID === user1.getID()) {
+                    let newScore = user1.getScore() + 1;
+                    user1.updateScore(newScore)
+                } else if (playerID === user2.getID()) {
+                    let newScore = user2.getScore() + 1;
+                    user2.updateScore(newScore)
                 }
                 break;
             } else {
                 // switch player
-                player = player_id === user1.getID() ? user2 : user1;
+                player = playerID === user1.getID() ? user2 : user1;
             }
         }
     }
@@ -209,11 +208,11 @@ function createGameboard() {
 
 };
 
-function createUser(mark, nick_name='Incognito') {
+function createUser(mark, nickName='Incognito') {
     
     let score = 0;
 
-    nick_name = (nick_name === 'Incognito') ? `${nick_name}#${id}` : nick_name;
+    nickName = (nickName === 'Incognito') ? `${nickName}#${id}` : nickName;
 
     if (mark === 'x' || mark === 'o') {
         mark = mark;
@@ -234,7 +233,7 @@ function createUser(mark, nick_name='Incognito') {
         score = newScore;
     }
 
-    getName = () => nick_name;
+    getName = () => nickName;
     getMark = () => mark;
     getScore = () => score;
     getID = () => id;
@@ -252,6 +251,6 @@ function createUser(mark, nick_name='Incognito') {
 
 // Testing Purposes
 const gameboard = createGameboard();
-const user1 = createUser('x', nick_name='Khashayar');
-const user2 = createUser('o', nick_name='Majid');
-// const user3 = createUser('a', nick_name='Ali');
+const user1 = createUser('x', nickName='Khashayar');
+const user2 = createUser('o', nickName='Majid');
+// const user3 = createUser('a', nickName='Ali');
